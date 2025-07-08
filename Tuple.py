ac8bba401590152c0f1cc3b2614100eabc5b9263
aravind <aravindravulapati20@gmail.com>
2025-07-08 19:43:20 +0530

    tuple files

---
+++ Tuple.py
@@ -0,0 +1,9 @@
+thistuple = ("apple","banana","cherry")
+print(thistuple)
+
+thistuple = ("apple", "banana", "cherry")
+print(thistuple[1])
+
+tuple = ("apple","banana","cherry")
+tuple [1]= "rose"
+print(tuple)
