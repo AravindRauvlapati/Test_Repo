e7ec6110f25a60a9b27d844c24d662de295c9764
aravind <aravindravulapati20@gmail.com>
2025-07-08 19:42:08 +0530

    commmiting list and tuple and dist items

---
+++ Dict.py
@@ -0,0 +1,12 @@
+thisdict = {
+    "brand":"Ford",
+    "model":"Mustang",
+    "year":1964
+}
+thisdist ={
+    "brand":"Ford",
+    "model":"mustang",
+    "year": 1897
+}
+x= thisdict["model"]
+print(x)
---
+++ List.py
@@ -0,0 +1,20 @@
+list = ["item1","item2","item2"]
+print(list)
+
+#Access List Items
+thislist = ["apple", "banana", "cherry"]
+print(thislist[1])
+
+#change list items
+thislist = ["apple", "banana", "cherry"]
+thislist[1] = "blackcurrant"
+print(thislist)
+
+
+#Add list item
+thislist = ["apple", "banana", "cherry"]
+thislist.append("orange")
+print(thislist)
+
+
+
