192dec07933cf8dfe7dca6123d0a11fd413efcaf
aravind <aravindravulapati20@gmail.com>
2025-07-08 19:22:38 +0530

    python value commiting

---
+++ Demo1.py
@@ -0,0 +1,16 @@
+x, y, z = "Orange", "Banana", "Cherry"
+print(x)
+print(y)
+print(z)
+
+x= "awesome"
+
+def myfunc():
+    print("pythin is" + x)
+myfunc()
+
+x="very bad"
+y="Avg"
+
+mylist = ["rose","banana","apple"]
+print(mylist)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Test_Repo
 Test project for web_Development
+
+Aravind_ Web Development 
